e58fced81882716cee5309e81a88c069306d5f45
Trevor Harmon <trevor.harmon@shopify.com>
2020-01-24 09:44:26 -0800

    Updated dependencies and wrapped thumbnail in button

--- src/templates/components/Thumbnail/Thumbnail.js
+++ src/templates/components/Thumbnail/Thumbnail.js
@@ -4,14 +4,19 @@ import Img from "gatsby-image"
 
 export const Thumbnail = ({ src, onClick }) => {
   return (
-    <div
+    <button
       sx={{
         cursor: "pointer",
         border: "1px solid gray",
         padding: 1,
+        "&:focus": {
+          outline: "none",
+          borderColor: "black",
+        },
       }}
+      onClick={onClick}
     >
-      <Img fluid={src.localFile.childImageSharp.fluid} onClick={onClick} />
-    </div>
+      <Img fluid={src.localFile.childImageSharp.fluid} />
+    </button>
   )
 }
